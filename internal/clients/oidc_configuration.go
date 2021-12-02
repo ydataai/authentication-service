92bb541add82fb8fe737b5c7d3dd2e81b5295f85
wsalles <wallace_robinson@hotmail.com>
2021-12-02 02:38:57 -0300

    unused properties removed

--- internal/clients/oidc_configuration.go
+++ internal/clients/oidc_configuration.go
@@ -1,20 +1,17 @@
 package clients
 
 import (
-	"time"
-
 	"github.com/kelseyhightower/envconfig"
 	"github.com/ydataai/authentication-service/internal/handlers"
 )
 
 // OIDCConfiguration defines a struct with required environment variables for a OpenID Connect
 type OIDCConfiguration struct {
-	ClientID           string        `envconfig:"CLIENT_ID" required:"true"`
-	ClientSecret       string        `envconfig:"CLIENT_SECRET" required:"true"`
-	OIDProviderURL     string        `envconfig:"OIDC_PROVIDER_URL" required:"true"`
-	OIDCRedirectURL    string        `envconfig:"OIDC_REDIRECT_URL" required:"true"`
-	OIDCScopes         []string      `envconfig:"OIDC_SCOPES" default:"openid,profile,email" split_words:"true"`
-	HTTPRequestTimeout time.Duration `envconfig:"HTTP_REQUEST_TIMEOUT" default:"30s"`
+	ClientID        string   `envconfig:"CLIENT_ID" required:"true"`
+	ClientSecret    string   `envconfig:"CLIENT_SECRET" required:"true"`
+	OIDProviderURL  string   `envconfig:"OIDC_PROVIDER_URL" required:"true"`
+	OIDCRedirectURL string   `envconfig:"OIDC_REDIRECT_URL" required:"true"`
+	OIDCScopes      []string `envconfig:"OIDC_SCOPES" default:"openid,profile,email" split_words:"true"`
 }
 
 // LoadFromEnvVars from the OIDC
--- internal/controllers/rest_controller_configuration.go
+++ internal/controllers/rest_controller_configuration.go
@@ -8,14 +8,8 @@ import (
 
 // RESTControllerConfiguration defines a struct with required environment variables for rest controller
 type RESTControllerConfiguration struct {
-	UserID             string        `envconfig:"USER_ID" required:"true"`
-	UserIDPrefix       string        `envconfig:"USER_ID_PREFIX" default:""`
 	OIDCCallbackURL    string        `envconfig:"OIDC_CALLBACK_URL" default:"/auth/oidc/callback"`
 	AuthServiceURL     string        `envconfig:"AUTHSERVICE_URL" default:"/auth"`
-	LogoutURL          string        `envconfig:"LOGOUT_URL" default:"/logout"`
-	AfterLoginURL      string        `envconfig:"AFTER_LOGIN_URL" default:"/"`
-	AfterLogoutURL     string        `envconfig:"AFTER_LOGOUT_URL" default:"/login"`
-	SessionMaxAge      int           `envconfig:"SESSION_MAX_AGE" default:"604800"`
 	HTTPRequestTimeout time.Duration `envconfig:"HTTP_REQUEST_TIMEOUT" default:"30s"`
 }
 
